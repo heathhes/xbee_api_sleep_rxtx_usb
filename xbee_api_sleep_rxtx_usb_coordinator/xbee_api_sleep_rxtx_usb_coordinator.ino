42bd72d99537bcd44714cdc89103196aade949cc
heath <heathhescock@gmail.com>
2020-04-13 21:10:26 -0400

    added parsing state machine. works with two remote xbees, not handled and no response, just print rx'd array

--- xbee_api_sleep_rxtx_usb_coordinator/xbee_api_sleep_rxtx_usb_coordinator.ino
+++ xbee_api_sleep_rxtx_usb_coordinator/xbee_api_sleep_rxtx_usb_coordinator.ino
@@ -11,6 +11,12 @@ Xbee_lib m_xbee;
 
 millisDelay m_system_timer;
 
+uint8_t m_parser_state = PARSE::SOM;
+uint8_t m_parser_cnt = 0;
+uint8_t m_msg_array[21] = {};
+struct Msg_data m_msg_data;
+
+
 uint8_t m_dest_addr = 0;
 uint8_t m_tx_count[6] = {};
 uint8_t m_tx_array[] = {0x7E, // SOM
@@ -71,11 +77,13 @@ void handle_wireless()
   while(Serial.available())
   {
     rx_array[x] = Serial.read();
-    ss.print(rx_array[x], HEX);
-    ss.print(", ");
+
+    process_byte(rx_array[x]);
+//    ss.print(rx_array[x], HEX);
+//    ss.print(", ");
 
     x++;
-    new_rx = true;
+    //new_rx = true;
   }
 
   if(new_rx)
@@ -135,6 +143,154 @@ void handle_wireless()
 
 //////////////////////////////////////////////////////////////////////
 
+void process_byte(uint8_t rx_byte)
+{
+  switch(m_parser_state)
+  {
+    case PARSE::SOM :
+      ss.println("SOM: ");
+      if(rx_byte == 0x7E)
+      {
+        reset_parser();
+        ss.print("SOM 0x7E: ");
+        ss.println(m_parser_cnt);
+
+        m_msg_array[m_parser_cnt] = rx_byte;
+        m_parser_state = PARSE::LENGTH;
+        m_parser_cnt++;
+      }
+      break;
+
+    case PARSE::LENGTH :
+      ss.print("LENGTH: ");
+      ss.println(m_parser_cnt);
+      m_msg_array[m_parser_cnt] = rx_byte;
+      if(m_parser_cnt == RX::LSB_LEN)
+      {
+        m_msg_data.length = rx_byte;
+        m_parser_state = PARSE::FRAME_TYPE;
+      }
+      m_parser_cnt++;
+      break;
+
+    case PARSE::FRAME_TYPE :
+      ss.print("FRAME_TYPE: ");
+      ss.println(m_parser_cnt);
+      m_msg_array[m_parser_cnt] = rx_byte;
+      m_msg_data.frame_type = rx_byte;
+      m_parser_state = PARSE::SOURCE;
+      m_parser_cnt++;
+      break;
+
+    case PARSE::SOURCE :
+      ss.print("SOURCE: ");
+      ss.println(m_parser_cnt);
+      m_msg_array[m_parser_cnt] = rx_byte;
+      if(m_parser_cnt == RX::SRC_7)
+      {
+        m_msg_data.address = m_xbee.Get_address(rx_byte);
+        ss.print("Address: ");
+        ss.println(m_msg_data.address);
+        m_parser_state = PARSE::UNUSED;
+      }
+      m_parser_cnt++;
+      break;
+
+    case PARSE::UNUSED :
+      ss.print("UNUSED: ");
+      ss.println(m_parser_cnt);
+      m_msg_array[m_parser_cnt] = rx_byte;
+      if(m_parser_cnt == RX::RESV_1 + 2)  // RESV 1, RESV2, TX_OPT
+      {
+        m_parser_state = PARSE::PAYLOAD_CNT;
+      }
+      m_parser_cnt++;
+      break;
+
+    case PARSE::PAYLOAD_CNT :
+      ss.print("PAYLOAD CNT: ");
+      ss.println(m_parser_cnt);
+      m_msg_array[m_parser_cnt] = rx_byte;
+      m_msg_data.payload_cnt = rx_byte;
+      m_parser_state = PARSE::PAYLOAD_TYPE;
+      m_parser_cnt++;
+      break;
+
+    case PARSE::PAYLOAD_TYPE :
+      ss.print("PAYLOAD TYPE: ");
+      ss.println(m_parser_cnt);
+      m_msg_array[m_parser_cnt] = rx_byte;
+      m_msg_data.payload_id = rx_byte;
+      m_parser_state = PARSE::PAYLOAD;
+      m_parser_cnt++;
+      break;
+
+    case PARSE::PAYLOAD :
+      ss.print("PAYLOAD: ");
+      ss.println(m_parser_cnt);
+      static uint8_t j = 0;
+      m_msg_array[m_parser_cnt] = rx_byte;
+      m_msg_data.payload[j] = rx_byte;
+      j++;
+      if(m_parser_cnt == RX::PAYLOAD_2)
+      {
+        m_parser_state = PARSE::CHECKSUM;
+        j = 0;
+      }
+      m_parser_cnt++;
+      break;
+
+    case PARSE::CHECKSUM :
+    {
+      ss.print("CHECKSUM: ");
+      ss.println(m_parser_cnt);
+      m_msg_array[m_parser_cnt] = rx_byte;
+      ss.print("CHECKSUM array: ");
+      print_array(m_msg_array, sizeof(m_msg_array), true);
+
+      uint8_t cs = m_xbee.Get_checksum(m_msg_array, sizeof(m_msg_array));
+      if(cs == m_msg_array[sizeof(m_msg_array) - 1])
+      {
+        m_msg_data.valid = true;
+        print_msg(m_msg_data, sizeof(m_msg_data.payload));
+      }
+      else
+      {
+        ss.println("Invalid checksum");
+      }
+      ss.print("Parser count: ");
+      ss.println(m_parser_cnt);
+      reset_parser();
+      break;
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+
+void reset_parser()
+{
+  m_xbee.Clear_array(m_msg_array, sizeof(m_msg_array));
+  m_parser_cnt = 0;
+  m_parser_state = PARSE::SOM;
+  clear_msg(m_msg_data);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+void clear_msg(struct Msg_data& msg)
+{
+  msg.valid = false;
+  msg.frame_type = 0;
+  msg.address = 0;
+  msg.length = 0;
+  msg.payload_cnt = 0;
+  msg.payload_id = 0;
+  msg.payload[3] = {};
+};
+
+//////////////////////////////////////////////////////////////////////
+
 void print_array(uint8_t array[], uint8_t len, bool hex)
 {
   if(hex)
@@ -161,15 +317,18 @@ void print_array(uint8_t array[], uint8_t len, bool hex)
 
 void print_msg(struct Msg_data msg, uint8_t len_payload)
 {
-  ss.print("Address: ");
-  ss.println(msg.address, HEX);
   ss.print("Length: ");
   ss.println(msg.length, HEX);
+  ss.print("Frame type: ");
+  ss.println(msg.frame_type, HEX);
+  ss.print("Address: ");
+  ss.println(msg.address, HEX);
   ss.print("Count: ");
-  ss.println(msg.count, HEX);
-  ss.print("Type_id: ");
-  ss.println(msg.type_id, HEX);
+  ss.println(msg.payload_cnt, HEX);
+  ss.print("Payload id: ");
+  ss.println(msg.payload_id, HEX);
   ss.print("Payload: ");
   print_array(msg.payload, len_payload, false);
+  ss.println();
 }
 
